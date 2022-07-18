f283b1a03ff75ea203512cfabc7427de6deb703a
Cyrillo46 <yahooisbetter46@gmail.com>
2022-07-17 22:59:11 -0400

    cleared comment from js file

--- app.js
+++ app.js
@@ -1,16 +1,7 @@
-/*
 fetch("https://dog.ceo/api/breeds/image/random")
   .then((res) => res.json())
-  .then(data => {
-    document.getElementById("image-container").innerHTML = `
-      <img src="${data.message}" alt="picture of random dog" />
-`;
-  });
-  */
-fetch("https://dog.ceo/api/breeds/image/random")
-  .then((response) => response.json())
   .then((data) => {
     document.getElementById("image-container").innerHTML = `
-            <img src="${data.message}" />
-        `;
+      <img src="${data.message}" alt="picture of random dog" />
+      `;
   });
