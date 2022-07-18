eeff1ab847fe2099ab0229e2f6d01f782ddaafe8
Cyrillo46 <yahooisbetter46@gmail.com>
2022-07-18 15:20:01 -0400

    Wiring up button, will complete later

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-
+document.getElementById("idea-btn").addEventListener("click", function () {
+  fetch("https://www.boredapi.com/api/activity/")
+    .then((res) => res.json())
+    .then((data) => {
+      document.getElementById("main-display").textContent = data.activity;
+    });
+});
