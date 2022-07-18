5043fc8be5760baa276fde617b91bb00a02a88d2
Cyrillo46 <yahooisbetter46@gmail.com>
2022-07-18 18:42:59 -0400

    fixed fetch API address

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 document.getElementById("idea-btn").addEventListener("click", function () {
-  fetch("https://www.boredapi.com/api/activity/")
+  fetch("http://www.boredapi.com/api/activity/")
     .then((res) => res.json())
     .then((data) => {
       document.getElementById("main-display").textContent = data.activity;
