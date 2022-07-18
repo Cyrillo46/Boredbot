90e545d316f401e53470cc0fb8c299edf3af2767
Cyrillo46 <yahooisbetter46@gmail.com>
2022-07-18 11:29:00 -0400

    Added some styling to stylesheet

--- app.js
+++ app.js
@@ -1,7 +1,1 @@
-fetch("https://dog.ceo/api/breeds/image/random")
-  .then((res) => res.json())
-  .then((data) => {
-    document.getElementById("image-container").innerHTML = `
-      <img src="${data.message}" alt="picture of random dog" />
-      `;
-  });
+
